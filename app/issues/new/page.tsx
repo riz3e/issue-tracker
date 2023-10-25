813d9a746e193fae1976d89ca7ad0b42c11fbb80
qrob <Abdraximm@gmail.com>
2023-10-25 18:07:39 +0600

    0.0.9 client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod'; 
 import { createIssueSchema } from '@/app/createIssueSchema';
 import z from 'zod';
+import ErrorMessage from '@/app/components/errormessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 const NewIssuePage = () => {
@@ -39,10 +40,9 @@ const NewIssuePage = () => {
                     <span className="label-text-alt"></span>
                 </label> */}
                 </div>
-                {errors.title && <div className="alert alert-error">
-                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                <span>{errors.title.message}</span>
-            </div>}
+                <ErrorMessage>
+                    {errors.title?.message}
+                </ErrorMessage>
                 <div className="form-control my-5 max-w-xl">
                     <label className="label">
                         <span className="label-text">Description of the issue</span>
@@ -53,10 +53,9 @@ const NewIssuePage = () => {
                         {/* <span className="label-text-alt">Alt label</span> */}
                     </label>
                 </div>
-                {errors.description && <div className="alert alert-error">
-                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                <span>{errors.description.message}</span>
-            </div>}
+                <ErrorMessage>
+                    {errors.description?.message}
+                </ErrorMessage>
                 <button className="btn btn-outline btn-primary">Submit new issue</button>
             </form>
         </div>
