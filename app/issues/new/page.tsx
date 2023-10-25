1d9d78297d3b39c6289c8fb228304801491a1cd8
qrob <Abdraximm@gmail.com>
2023-10-25 17:59:27 +0600

    0.0.9 client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,23 +3,21 @@ import React, { useState } from 'react'
 import { useForm, SubmitHandler, Form } from "react-hook-form"
 import axios from 'axios'
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod'; 
+import { createIssueSchema } from '@/app/createIssueSchema';
+import z from 'zod';
 
-interface IssueForm {
-    title: string;
-    description: string;
-};
-
+type IssueForm = z.infer<typeof createIssueSchema>;
 const NewIssuePage = () => {
-    const { register, handleSubmit } = useForm<IssueForm>();
+    const { register, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    });
     const router = useRouter();
     const [error, setError] = useState('')
 
     return (
         <div className='max-w-xl mx-auto'>
-            {error && <div className="alert alert-error">
-                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                <span>{error}</span>
-            </div>}
+            
             <form onSubmit={handleSubmit(async (data) => {
                 try {
                     await axios.post('/api/issues', data)
@@ -41,6 +39,10 @@ const NewIssuePage = () => {
                     <span className="label-text-alt"></span>
                 </label> */}
                 </div>
+                {errors.title && <div className="alert alert-error">
+                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span>{errors.title.message}</span>
+            </div>}
                 <div className="form-control my-5 max-w-xl">
                     <label className="label">
                         <span className="label-text">Description of the issue</span>
@@ -51,6 +53,10 @@ const NewIssuePage = () => {
                         {/* <span className="label-text-alt">Alt label</span> */}
                     </label>
                 </div>
+                {errors.description && <div className="alert alert-error">
+                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span>{errors.description.message}</span>
+            </div>}
                 <button className="btn btn-outline btn-primary">Submit new issue</button>
             </form>
         </div>
